1afd97bdcdd4bfbc0419b1c97831fbf3b9b3d4d6
Johannes Stephan Tafelmaier <johannes@tafelmaier-bammer.de>
2026-04-23 17:53:12 +0200

    -

--- src/md_create_html.py
+++ src/md_create_html.py
@@ -12,103 +12,6 @@ def generate_htmls():
     list_dicts_factions = md_shared.get_dict_setting("data_factions.json") \
         ["data"]
 
-    # TODO integrate
-    def get_text_html_data_unit(
-        dict_unit:typing.Dict,
-        name_faction:str,
-        text_side:str,
-        int_index_unit:int = 0):
-
-        text_parameters_functions = "'" \
-            + text_side \
-            + "', " \
-            + int_index_unit \
-                .__str__()
-
-        def get_text_html_attack(
-            pair_dict_attack:typing.Tuple[int, typing.Dict]):
-
-            int_index_attack, \
-            dict_attack = pair_dict_attack
-
-            return "<div class=\"model_property attack\" onclick=\"toggle_select_attack(" \
-                + text_parameters_functions \
-                + ", " \
-                + int_index_attack \
-                    .__str__() \
-                + ")\"><div class=\"icon\">" \
-                + dict_attack \
-                    ["range"] \
-                + "</div><div class=\"skillbar\"><div style=\"width: " \
-                +  (dict_attack \
-                        ["strength"]
-                        * 8) \
-                    .__str__() \
-                + "px;\"/></div><div class=\"value\">" \
-                + dict_attack \
-                    ["strength"] \
-                    .__str__() \
-                + "</div><div class=\"type\">" \
-                + dict_attack \
-                    ["type"] \
-                + (" heavy" if dict_attack["heavy"] else "") \
-                + "</div></div>"
-
-        path_image_unit = "/" \
-            .join(
-                [
-                    md_shared.get_text_path_images_faction(name_faction),
-                    "units",
-                    dict_unit \
-                        ["name"] \
-                        + ".png"])
-
-        text_html_rows_attacks = "" \
-            .join(
-                map(
-                    get_text_html_attack,
-                    enumerate(
-                        dict_unit
-                            ["attacks"])))
-
-        return "<div class=\"model\" title=\"[" \
-            + dict_unit["type_unit"] \
-            + "], " \
-            + dict_unit \
-                ["points_per_model"] \
-                .__str__() \
-            + " points per model.\"><div class=\"image_unit\" style=\"background-image: url('" \
-            + path_image_unit \
-            + "')\"><div class=\"data_unit\"><div class=\"model_property name\">" \
-            + dict_unit \
-                ["name"] \
-            + "</div><div class=\"model_property armor\"><div class=\"icon\">⛊</div><div class=\"skillbar\"><div style=\"width: " \
-            +  (dict_unit \
-                    ["armor"]
-                    * 8) \
-                .__str__() \
-            + "px;\"/></div><div class=\"value\">" \
-            + dict_unit \
-                ["armor"] \
-                .__str__() \
-            + "</div><div class=\"type\">" \
-            + dict_unit \
-                ["type_armor"] \
-            + "</div></div><div class=\"model_property movement\" onclick=\"set_inactive(" \
-            + text_parameters_functions \
-            + ")\"><div class=\"icon\">🡆</div><div class=\"skillbar\"><div style=\"width: " \
-            +  (dict_unit \
-                    ["move"]
-                    * 1.5) \
-                .__str__() \
-            + "px;\"/></div><div class=\"value\">" \
-            + dict_unit \
-                ["move"] \
-                .__str__() \
-            + "</div></div>" \
-            + text_html_rows_attacks \
-            + "</div></div></div>"
-
     def get_text_html_faction_rules(
         text_side):
 
@@ -161,6 +64,35 @@ def generate_htmls():
                     + int_index_unit \
                         .__str__()
 
+                def get_text_html_attack(
+                    pair_dict_attack:typing.Tuple[int, typing.Dict]):
+
+                    int_index_attack, \
+                    dict_attack = pair_dict_attack
+
+                    return "<div class=\"model_property attack\" onclick=\"toggle_select_attack(" \
+                        + text_parameters_functions \
+                        + ", " \
+                        + int_index_attack \
+                            .__str__() \
+                        + ")\"><div class=\"icon\">" \
+                        + dict_attack \
+                            ["range"] \
+                        + "</div><div class=\"skillbar\"><div style=\"width: " \
+                        +  (dict_attack \
+                                ["strength"]
+                                * 8) \
+                            .__str__() \
+                        + "px;\"/></div><div class=\"value\">" \
+                        + dict_attack \
+                            ["strength"] \
+                            .__str__() \
+                        + "</div><div class=\"type\">" \
+                        + dict_attack \
+                            ["type"] \
+                        + (" heavy" if dict_attack["heavy"] else "") \
+                        + "</div></div>"
+
                 return "<div class=\"unit_faction unselected\" initial_health=\"0\" current_health=\"0\"><div class=\"unit_count_modifier\"><div class=\"modify_count\" onclick=\"modify_count_models('" \
                     + text_side \
                     + "', " \
@@ -187,13 +119,57 @@ def generate_htmls():
                     + text_parameters_functions \
                     + ")\" onclick=\"apply_preview(" \
                     + text_parameters_functions \
-                    + ")\">0.00</div></div><div class=\"health_bar\"><div class=\"section difference\"></div><div class=\"section remaining\" style=\"height: 100%;\"></div></div></div>" \
-                    + get_text_html_data_unit(
-                        dict_unit=dict_unit,
-                        name_faction=name_faction,
-                        text_side=text_side,
-                        int_index_unit=int_index_unit) \
-                    + "</div>"
+                    + ")\">0.00</div></div><div class=\"health_bar\"><div class=\"section difference\"></div><div class=\"section remaining\" style=\"height: 100%;\"></div></div></div><div class=\"model\" title=\"[" \
+                    + dict_unit \
+                        ["type_unit"] \
+                    + "], " \
+                    + dict_unit \
+                        ["points_per_model"] \
+                        .__str__() \
+                    + " points per model.\"><div class=\"image_unit\" style=\"background-image: url('" \
+                    + "/" \
+                        .join(
+                            [
+                                md_shared.get_text_path_images_faction(name_faction),
+                                "units",
+                                dict_unit \
+                                    ["name"] \
+                                    + ".png"]) \
+                    + "')\"><div class=\"data_unit\"><div class=\"model_property name\">" \
+                    + dict_unit \
+                        ["name"] \
+                    + "</div><div class=\"model_property armor\"><div class=\"icon\">⛊</div><div class=\"skillbar\"><div style=\"width: " \
+                    +  (dict_unit \
+                            ["armor"]
+                            * 8) \
+                        .__str__() \
+                    + "px;\"/></div><div class=\"value\">" \
+                    + dict_unit \
+                        ["armor"] \
+                        .__str__() \
+                    + "</div><div class=\"type\">" \
+                    + dict_unit \
+                        ["type_armor"] \
+                    + "</div></div><div class=\"model_property movement\" onclick=\"set_inactive(" \
+                    + text_parameters_functions \
+                    + ")\"><div class=\"icon\">🡆</div><div class=\"skillbar\"><div style=\"width: " \
+                    +  (dict_unit \
+                        ["move"]
+                        * 1.5) \
+                        .__str__() \
+                    + "px;\"/></div><div class=\"value\">" \
+                    + dict_unit \
+                        ["move"] \
+                        .__str__() \
+                    + "</div></div>" \
+                    + "" \
+                        .join(
+                            map(
+                                get_text_html_attack,
+                                enumerate(
+                                    dict_unit
+                                        ["attacks"]))) \
+                    + "</div></div></div></div>"
 
             return "<div class=\"faction constructor invisible " \
                 + name_faction \
