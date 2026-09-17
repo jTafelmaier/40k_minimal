2adf7c9afc037ed75c1815e5f908c098611fa2e7
Johannes Stephan Tafelmaier <johannes@tafelmaier-bammer.de>
2026-09-17 13:37:52 +0200

    -

--- resources/scripts.js
+++ resources/scripts.js
@@ -363,10 +363,9 @@ function toggle_select_attack(
                 .innerText
                 .trim())
 
-            if (text_keywords_attack.includes("volume") && get_int_count_models(element_unit_attacked) == 1) {
-                return Math.floor(
-                        int_strength
-                            / 2)}
+            if (text_keywords_attack.includes("volume") && get_int_count_models(element_unit_attacked) >= 1) {
+                return int_strength
+                    * 2}
             else {
                 return int_strength
             }
